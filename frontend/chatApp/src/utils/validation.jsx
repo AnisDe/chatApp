44f29637d068615597162953c9eede3b1d8c0bc7
Anis Demni <anisdemni@gmail.com>
2025-10-04 03:27:03 +0100

    Add frontend validation utilities

---
+++ frontend/chatApp/src/utils/validation.jsx
@@ -0,0 +1,11 @@
+export const validatePassword = (password) => {
+  const errors = [];
+
+  if (password.length < 8) errors.push("At least 8 characters");
+  if (!/[A-Z]/.test(password)) errors.push("At least one uppercase letter");
+  if (!/[a-z]/.test(password)) errors.push("At least one lowercase letter");
+  if (!/[0-9]/.test(password)) errors.push("At least one number");
+  if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) errors.push("At least one special character");
+
+  return errors;
+};
