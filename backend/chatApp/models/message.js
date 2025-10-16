654d2b1bf3bcd014142d4a66df68f9e2a2391b2a
Anis Demni <anisdemni@gmail.com>
2025-10-16 01:16:23 +0100

    adding conversation model and updating message model

--- backend/chatApp/models/message.js
+++ backend/chatApp/models/message.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema(
   {
+    conversationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
     sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    message: { type: String, required: true }
+    message: { type: String, required: true },
   },
   { timestamps: true }
 );
-
-export default mongoose.model("message", messageSchema);
